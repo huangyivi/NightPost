556f67e50ff7aebf9bb74447588505d8c0ad9784
HYivisir <843702140@qq.com>
2021-05-07 15:19:54 +0800

    修改自适应布局

--- pages/myDream/editDream/editDream.js
+++ pages/myDream/editDream/editDream.js
@@ -3,6 +3,10 @@ Page({
   data: {
     statusBarHeight: app.globalData.statusBarHeight,
     capsuleHeight: 44,
+    access: ['公开','私密'],
+    accessIndex: 0,
+    tags: ['怪诞','美好','噩梦','平常'],
+    tagIndex: 0,
     detail: '锦瑟无端五十弦，一弦一柱思华年。庄生晓梦迷蝴蝶，望帝春心托杜鹃。沧海月明珠有泪，蓝田日暖玉生烟。此情可待成追忆？只是当时已惘然。锦瑟无端五十弦，一弦一柱思华年。庄生晓梦迷蝴蝶，望帝春心托杜鹃。沧海月明珠有泪，蓝田日暖玉生烟。此情可待成追忆？只是当时已惘然。锦瑟无端五十弦，一弦一柱思华年。'
   },
   onReady: function(){
@@ -11,6 +15,16 @@ Page({
       success: function(e){
         that.data.capsuleHeight = e.height;
       }
+    })
+  },
+  isAccessedChange(e) {
+    this.setData({
+      accessIndex : e.detail.value
+    })
+  },
+  isTagsChange(e) {
+    this.setData({
+      tagIndex : e.detail.value
     })
   }
 })
