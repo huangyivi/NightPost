8f7c355cc3d0742ab6d93bfbab384ad8c20d2fb3
HYivisir <843702140@qq.com>
2021-05-10 14:48:18 +0800

    完成录音功能

--- pages/myDream/published/published.js
+++ pages/myDream/published/published.js
@@ -2,7 +2,10 @@ const app = getApp();
 Page({
   data: {
     statusBarHeight: app.globalData.statusBarHeight,
-    capsuleHeight: 44
+    capsuleHeight: 44,
+    animationData : null,
+    hasEgg : true,
+    eggCtx: ' 梦见养羊，预示着你近期的运势很好，路上将会遇到自己很久没见的故人，两人有很多说不完的话。找工作者梦见养羊，预示着你近期的求职运势很好，自己的表现得到考官的欣赏，有机会获得一份好的工作。务员梦见养羊，预示着你近期的运势很好，自己的表现太好，得到上司的器重，有机会可以升职。找工作者梦见养羊，预示着你近期的求职运势很好，自己的表现得到考官的欣赏，有机会获得一份好的工作。公务员梦见养羊，预示着你近期的运势很好，自己的表现太好，得到上司的器重，有机会可以升职。找工作者梦见养羊，预示着你近期的求职运势很好，自己的表现得到考官的欣赏，有机会获得一份好的工作。公务员梦见养羊，预示着你近期的运势很好，自己的表现太好，得到上司的器重，有机会可以升职。'
   },
   onReady: function(){
     let that = this;
@@ -10,6 +13,16 @@ Page({
       success: function(e){
         that.data.capsuleHeight = e.height;
       }
+    })
+  },
+  onShow() {
+    let move = wx.createAnimation({
+      duration: 3000,
+      timingFunction: "ease"
+    })
+    move.opacity(1).step();
+    this.setData({
+      animationData : move.export()
     })
   },
   return(){
